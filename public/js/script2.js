6a08ef385727cf5471994cee0b0d6a53443187fe
Lakshya-Kapoor <Lakshya.Kapoor@iiitb.ac.in>
2024-06-01 12:01:47 +0530

    Changing url to room page

--- public/js/script2.js
+++ public/js/script2.js
@@ -3,5 +3,5 @@ let joinBtn = document.querySelector(".join-btn");
 joinBtn.addEventListener("click", () => {
   let inputElement = document.querySelector("input");
   console.log(inputElement.value);
-  window.location.href = `http://localhost:8080/room/${inputElement.value}`;
+  window.location.href = `https://tic-tac-toe-live.onrender.com/room/${inputElement.value}`;
 });
